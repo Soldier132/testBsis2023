116a1c8d97f1070798108f8d9ad80bd9b0f83aac
Soldier132 <126676533+Soldier132@users.noreply.github.com>
2023-03-10 11:34:09 +0800

    Update grades.php

--- grades.php
+++ grades.php
@@ -1,15 +1,19 @@
 <?php
-	setlocale(LC_ALL, 'ru_RU.UTF-8');
-	echo '<html lang="ru" xml:lang="ru" xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><style media="print" type="text/css">.noprint {display: none;}}</style></head><body style="margin-left: 100;">';
-$link = mysqli_connect('localhost:3311', 'moodleuser', 'moodle', 'moodle');
-if (!$link) {
-    die('Ошибка подключения (' . mysqli_connect_errno() . ') '
+    setlocale(LC_ALL, 'ru_RU.UTF-8');
+    echo '<html lang="ru" xml:lang="ru" xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><style media="print" type="text/css">.noprint {display: none;}}</style></head><body style="margin-left: 100;">';
+    $link = mysqli_connect('localhost:3311', 'moodleuser', 'moodle', 'moodle');
+    if (!$link) {
+        die('Ошибка подключения (' . mysqli_connect_errno() . ') '
             . mysqli_connect_error());
-}
-mysqli_set_charset($link, "utf8");
+    }
+    mysqli_set_charset($link, "utf8");
+    //strip_tags($_GET['attempt']);
+    //if (!$a = intval($a)) $a = 1    
+    //$attemptid = (int) $_GET['attempt'];
+
 if (is_numeric($_GET['attempt'])) {
 	echo '<span class="noprint"><center><p><input type="button" value="Назад" onClick="window.location=\'./quiz/review.php?attempt=' . $_GET['attempt'] . '\'"><input type="button" value="Печать" onclick="javascript:print()"></input></p></center></span>';
-	$result = mysqli_query($link, 'SELECT * FROM `mdl_quiz_attempts` WHERE `id`='.$_GET['attempt']);
+	$result = mysqli_query('SELECT * FROM `mdl_quiz_attempts` WHERE `id`='.$_GET['attempt']);
 	$row = mysqli_fetch_assoc($result);
 	$userid = $row['userid'];
 	$questions  = $row['layout'];
@@ -18,19 +22,17 @@ if (is_numeric($_GET['attempt'])) {
 	$timefinish_usr = $row['timefinish'];
 	$seconds = $timefinish_usr - $timestart_usr;
 	$time = floor($seconds/3600).":".sprintf("%02d", floor(($seconds - 3600*floor($seconds/3600))/60)).":".sprintf("%02d", $seconds - 60*floor(($seconds - 3600*floor($seconds/3600))/60) - 3600*floor($seconds/3600));
-	$result = mysqli_query($link, 'SELECT * FROM `mdl_quiz` WHERE `id`='.$row['quiz']);
+	$result = mysqli_query('SELECT * FROM `mdl_quiz` WHERE `id`='.$row['quiz']);
 	$row = mysqli_fetch_assoc($result);
 	$sumgrades_quiz = $row['sumgrades'];
-	$result = mysqli_query($link, 'SELECT * FROM `mdl_user` WHERE `id`='.$userid);
+	$result = mysqli_query('SELECT * FROM `mdl_user` WHERE `id`='.$userid);
 	$row = mysqli_fetch_assoc($result);
 	$usr = $row['firstname'].' '.$row['lastname'];	
 	$per = $sumgrades_usr/$sumgrades_quiz*100;
 	if ($per >= 75) {$grade='Отлично';} elseif ($per >= 60 && $per < 75) {$grade='Хорошо';} elseif ($per >= 50 && $per < 60) {$grade='Удовлетворительно';} else {$grade='Неудовлетворительно';} ;
 	echo
-'<tt><font size="1px">
+'<tt><font size="12px">
 <div align="center">
-				
-
 				<b>ПРОТОКОЛ № _________
 заседания комиссии по проверке знаний нормативно-технических документов</b><br>
 <br><br>
@@ -172,8 +174,9 @@ ________________________________________________________________________________
 		if ($que[$i]!=0) {
 			$num = $num+1;
 			echo '<br><br><b>Вопрос №'.$num.'</b><br>';
-			$slots_query = mysqli_query($conn, 'SELECT * FROM `mdl_question_attempts` WHERE `slot`='.$que[$i]);
-			while ($row_slots = mysqli_fetch_assoc($slots_query)) {
+			$slots = mysqli_query('SELECT * FROM `mdl_question_attempts` WHERE `slot`='.$que[$i]);
+			while ($row_slots = mysqli_fetch_assoc($slots)) {
+		
 				$step = mysqli_query('SELECT * FROM `mdl_question_attempt_steps` WHERE `userid`='.$userid.' AND `questionattemptid`='.$row_slots['id']);
 				while ($row_step = mysqli_fetch_assoc($step)) {
 					if ($row_step['state'] == 'gradedwrong') {
